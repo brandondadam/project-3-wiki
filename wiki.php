b2f69b77d2a8d46859b8ad6bea38ed1cadaddde6
bdadam <bdadam@mail.bradley.edu>
2015-11-09 01:22:54 -0600

    looks for query parameters in the url

--- wiki.php
+++ wiki.php
@@ -4,6 +4,11 @@ if (file_exists('wiki.txt')){
 } else {
 	$content = '(no content)';
 }
+
+if (isset($_Get['content'])) {
+	$content = $_Get['content'];
+	file_put_contents('wiki.txt', $content)
+}
 safe_content = htmlentities($content);
 echo $safe_content;
 ?>
