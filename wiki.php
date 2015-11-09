5744f45100b5b827793902d9b87a2651f78c4167
bdadam <bdadam@mail.bradley.edu>
2015-11-09 13:10:46 -0600

    messed with the div some more

--- wiki.php
+++ wiki.php
@@ -16,22 +16,14 @@
 				$content = $_GET['content'];
 				file_put_contents('wiki.txt', $content);
 			}
-			
+			$safe_content = htmlentities($content);
 
 			?>
+			<div id="content">
+				<?php echo $safe_content; ?>
+			</div>
 			<form action="wiki.php">
 				<textarea name="content" rows="8" cols"80">
-					<?php
-					$safe_content = htmlentities($content);
-					?>
-					<div id="content">
-						<?php echo $safe_content; ?>
-					</div>
-
-
-
-
-
 				</textarea>
 				<input type="submit" value="Save">
 			</form>
