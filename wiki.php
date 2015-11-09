1d40f856e6a38763e9a0512de479719c44f184fb
bdadam <bdadam@mail.bradley.edu>
2015-11-09 12:25:43 -0600

    restarted kind of, should be a blank page

--- wiki.php
+++ wiki.php
@@ -7,17 +7,12 @@
 	<body>
 		<?php
 			if (file_exists('wiki.txt')) {
-				$contnet = file_get_contents('wiki.txt');
+				$content = file_get_contents('wiki.txt');
 			} else {
 				$content = '(no content)';
 			}
-
-			if (isset($_Get['content'])) {
-				$content = $_Get['content'];
-				file_put_contents('wiki.txt', $content);
-			}
-			safe_content = htmlentities($content);
+			$safe_content = htmlentities($content);
 			echo $safe_content;
-		?>
+			?>
 	</body>
 </html>
