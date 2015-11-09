c0c15123dd2d32c723656ff69052fdbb1b3007f6
bdadam <bdadam@mail.bradley.edu>
2015-11-09 09:48:19 -0600

    add html doc setup

--- wiki.php
+++ wiki.php
@@ -1,14 +1,23 @@
-<?php
-if (file_exists('wiki.txt')){
-	$contnet  file_get_contents('wiki.txt');
-} else {
-	$content = '(no content)';
-}
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Wiki</title>
+	</head>
+	<body>
+		<?php
+			if (file_exists('wiki.txt')){
+				$contnet  file_get_contents('wiki.txt');
+			} else {
+				$content = '(no content)';
+			}
 
-if (isset($_Get['content'])) {
-	$content = $_Get['content'];
-	file_put_contents('wiki.txt', $content)
-}
-safe_content = htmlentities($content);
-echo $safe_content;
-?>
+			if (isset($_Get['content'])) {
+				$content = $_Get['content'];
+				file_put_contents('wiki.txt', $content)
+			}
+			safe_content = htmlentities($content);
+			echo $safe_content;
+		?>
+	</body>
+</html>
