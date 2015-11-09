baa95b5890094259cf60a5ad76c68088195d6527
bdadam <bdadam@mail.bradley.edu>
2015-11-09 13:30:35 -0600

    setup style sheet, and viewport in html

--- wiki.php
+++ wiki.php
@@ -2,7 +2,9 @@
 <html>
 	<head>
 		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width">
 		<title>Wiki</title>
+		<link rel="stylesheet" href="wiki.css">
 	</head>
 	<body>
 		<?php
