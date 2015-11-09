fad4c6ecd127a529f0ba91a8892ecc799e5e2506
bdadam <bdadam@mail.bradley.edu>
2015-11-09 13:05:23 -0600

    moved the php and div into the textarea

--- wiki.php
+++ wiki.php
@@ -20,14 +20,20 @@
 			echo $safe_content;
 			?>
 			<form action="wiki.php">
-				<textarea name="content" rows="8" cols"80"></textarea>
+				<textarea name="content" rows="8" cols"80">
+					<?php
+					$safe_content = htmlentities($content);
+					?>
+					<div id="content">
+						<?php echo $safe_content; ?>
+					</div>
+
+
+
+
+
+				</textarea>
 				<input type="submit" value="Save">
 			</form>
-			<?php
-			$safe_content = htmlentities($content);
-			?>
-			<div id="content">
-				<?php echo $safe_content; ?>
-			</div>
 	</body>
 </html>
