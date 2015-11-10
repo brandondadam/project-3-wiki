72a2f088552c0cd9852ce389ae4f89829855e9cf
bdadam <bdadam@mail.bradley.edu>
2015-11-09 21:54:08 -0600

    some debug stuff

--- wiki.php
+++ wiki.php
@@ -30,7 +30,9 @@
 			</div>
 			<script src="jquery-1.11.3.min.js"></script>
 			<script>
+
 				document.getElementById('wikiForm').onKeydown = function(e){
+					console.console.log("hello");
 					var charCode = e ? (e.which ? e.which: e.keycode): window.event.keycode;
 					console.log(charCode);
 					if (charCode == 13){
